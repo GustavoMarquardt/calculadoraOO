6f0fc9d070b243113092618d14d26b1c460a13d1
Gustavo Marquardt <gustavo.marquardt@unifei.edu.br>
2022-03-18 15:42:36 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+    class Calculadora{
+        private $n;
+        public function __construct($numeroInicial){
+            $this->n = $numeroInicial;
+        }
+
+        public function somar($n1){
+            $this->n += $n1;
+            return $this;
+        }
+        public function subtrair($n1){
+            $this->n -= $n1;
+            return $this;
+        }
+        public function multiplicar($n1){
+            $this->n *= $n1;
+            return $this;
+        }
+        public function dividir($n1){
+            $this->n /= $n1;
+            return $this;
+        }
+
+        public function resultado(){
+            return $this->n;
+        }
+    }
+
+    $calc = new Calculadora(10);
+    $calc->somar(5)->multiplicar(2)->subtrair(3)->dividir(2);
+    $resultado = $calc->resultado();
+    echo "Resultado é : ".$resultado;
+?>
